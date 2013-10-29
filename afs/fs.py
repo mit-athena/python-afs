1c5ab04603b01d3454c33970de75752ed68f102c
Jonathan Reed <jdreed@mit.edu>
2013-10-29 14:53:19 -0400

    Add the high-level lsmount function
    Change the import statement for afs._fs to import *, now that
    we have defined __all__.
    
    Add the high-level lsmount function.

--- afs/fs.py
+++ afs/fs.py
@@ -1,6 +1,7 @@
 import errno
+import os.path
 from afs import _fs
-from afs._fs import whichcell
+from afs._fs import *
 
 def inafs(path):
     """Return True if a path is in AFS."""
@@ -11,3 +12,17 @@ def inafs(path):
             return False
 
     return True
+
+def lsmount(path):
+    """Return a volume name for a mountpoint."""
+    # os.path.realpath will take care of ensuring we don't
+    # get back anything that ends in . or .., and will
+    # strip off any trailing slash
+    (dirname, basename) = os.path.split(os.path.realpath(path))
+    try:
+        return _lsmount(dirname, basename)
+    except OSError as e:
+        if e.errno == errno.EINVAL:
+            return None
+        else:
+            raise
