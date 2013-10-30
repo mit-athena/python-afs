2cf0bfd86d1e074e7ef7b8de78114a5a5029baf9
Jonathan Reed <jdreed@mit.edu>
2013-10-30 11:15:42 -0400

    Import the submodules in __init__.py
    Import the submodules, so that people using the package don't
    have to import afs and then afs.fs, because that's just dumb

--- afs/__init__.py
+++ afs/__init__.py
@@ -1,4 +1,9 @@
 import logging
+# Make the submodules trivally available when
+# you type "import afs"
+import afs.fs
+import afs.acl
+import afs.pts
 
 # Basic logging support for this package.
 # Add a NullHandler to avoid "No handlers could be found" error
